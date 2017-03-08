41f0b71a51ee9e20f4bd7086c79055f01b48a49f
root <root@localhost.localdomain>
2017-03-08 18:41:43 -0500

    First commit

---
+++ design.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'design.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(218, 280)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setEnabled(True)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.listWidget = QtGui.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(5, 5, 201, 231))
+        self.listWidget.setObjectName(_fromUtf8("listWidget"))
+        self.btnBrowse = QtGui.QPushButton(self.centralwidget)
+        self.btnBrowse.setGeometry(QtCore.QRect(5, 240, 201, 26))
+        self.btnBrowse.setIconSize(QtCore.QSize(24, 24))
+        self.btnBrowse.setObjectName(_fromUtf8("btnBrowse"))
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.btnBrowse.setText(_translate("MainWindow", "Pick a folder", None))
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Qt-tut',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='local',
+    author_email='',
+    description=''
+)
